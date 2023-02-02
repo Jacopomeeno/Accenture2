fe4c5bafe631faaaafd93fa12cc0673140abdaf9
Jacopomeeno <100274199+Jacopomeeno@users.noreply.github.com>
2023-02-02 16:35:27 +0100

    Problema view associa impegno

--- webapp/controller/aImpegno.controller.js
+++ webapp/controller/aImpegno.controller.js
@@ -1,6 +1,6 @@
-sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
+sap.ui.define([
+        "./BaseController",
+        
     ],
     function (BaseController) {
         "use strict";
@@ -10,7 +10,32 @@ sap.ui.define(
             },
             onBackButton: function(){
                 this.getOwnerComponent().getRouter().navTo("iconTabBar");
+            },
+
+            onSearch: function (oEvent) {
+                // this.onCallHeader()
+        
+                var that = this;
+                this.getView().byId("HeaderNIAssImp").setVisible(true);
+        
+                var that = this;
+                var oMdlAImp = new sap.ui.model.json.JSONModel();
+                this.getOwnerComponent().getModel().read("/HeaderNISet", {
+                    success: function (data) {
+                        oMdlAImp.setData(data.results);
+                        that.getView().getModel("temp").setProperty('/HeaderNISet', data.results)
+                    },
+                    error: function (error) {
+                        var e = error;
+                    }
+                });
+        
+                this.getOwnerComponent().setModel(oMdlAImp, "HeaderNIAssImp");
+                //sap.ui.getCore().TableModel = oMdlW;
+        
             }
         });
-    }
+    },
+
+    
 );
