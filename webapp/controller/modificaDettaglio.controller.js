17e3aeae56d7cc1f0d2b5370fdf33480d73f43b2
Jacopomeeno <100274199+Jacopomeeno@users.noreply.github.com>
2023-02-07 17:31:25 +0100

    Prova controller modifica dettaglio

--- webapp/controller/modificaDettaglio.controller.js
+++ webapp/controller/modificaDettaglio.controller.js
@@ -38,6 +38,7 @@ sap.ui.define(
             onSaveDati: function () {
                 /*update operation*/
                 var that = this
+                var oItems = that.getView().byId("idModificaDettaglio").getBinding("items").oList;
                 var oggSpesa = this.getView().byId("idModificaDettaglio").mBindingInfos.items.binding.oModel.oData[0].ZoggSpesa
                 MessageBox.warning("Sei sicuro di voler modificare la NI?", {
                     actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
@@ -45,20 +46,37 @@ sap.ui.define(
                     onClose: function (oAction) {
                         if (oAction === sap.m.MessageBox.Action.YES) {
                             var oModel = that.getView().getModel();
-                            var editSpesa = {
-                                ZoggSpesa: oggSpesa
-                            };
-                            oModel.update("/HeaderNISet('ZoggSpesa='" + editSpesa.ZoggSpesa + "')", editSpesa, {
-                                method: "PUT",
-                                success: function (data) {
-                                    //console.log("success");
-                                    MessageBox.success("Operazione eseguita con successo")
-                                },
-                                error: function (e) {
-                                    //console.log("error");
-                                    MessageBox.error("Operazione non eseguita")
-                                }
-                            });
+                            // var editSpesa = {
+                            //     ZoggSpesa: oggSpesa
+                            // };
+                            for(var i=0; i<oItems.length; i++){
+                                var item = oItems[i];
+                                var editSpesa = {
+                                    ZoggSpesa: item.ZoggSpesa
+                                };
+                                oModel.update("/HeaderNISet('Bukrs='"+item.Bukrs+"',Gjahr='"+item.Gjahr+"',Zamministr='"+item.Zamministr+"',ZchiaveNi='"+item.ZchiaveNi+"',ZidNi='"+item.ZidNi+"',ZRagioCompe='"+item.ZRagioCompe+"'')", editSpesa, {
+                                    // method: "PUT",
+                                    success: function (data) {
+                                        //console.log("success");
+                                        MessageBox.success("Operazione eseguita con successo")
+                                    },
+                                    error: function (e) {
+                                        //console.log("error");
+                                        MessageBox.error("Operazione non eseguita")
+                                    }
+                                });
+                            }
+                            // oModel.update("/HeaderNISet('Bukrs='c123',Gjahr='2023',Zamministr='abe',ZchiaveNi='2023-abe-15-1',ZidNi='1',ZRagioCompe='15'')", editSpesa, {
+                            //     // method: "PUT",
+                            //     success: function (data) {
+                            //         //console.log("success");
+                            //         MessageBox.success("Operazione eseguita con successo")
+                            //     },
+                            //     error: function (e) {
+                            //         //console.log("error");
+                            //         MessageBox.error("Operazione non eseguita")
+                            //     }
+                            // });
                         }
                     }
                 });
