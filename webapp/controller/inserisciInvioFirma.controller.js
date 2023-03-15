bcc3ce4304774a3ee8816781441148aff1dbfb67
Looka23 <lucapelliccioni23@gmail.com>
2023-03-15 11:31:17 +0100

    Aggiunta Header/Tabella InserisciFirma

--- webapp/controller/inserisciInvioFirma.controller.js
+++ webapp/controller/inserisciInvioFirma.controller.js
@@ -1,14 +1,17 @@
 sap.ui.define(
     [
         "./BaseController",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator",
         'sap/ui/model/json/JSONModel',
         'sap/ui/export/Spreadsheet',
-        "sap/ui/core/library"
+        "sap/ui/core/library",
+        "project1/model/DateFormatter",
     ],
     /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-    function (BaseController, JSONModel, Spreadsheet, CoreLibrary) {
+    function (BaseController, Filter, FilterOperator, JSONModel, Spreadsheet, CoreLibrary, DateFormatter) {
         "use strict";
         var EdmType = sap.ui.export.EdmType
 
@@ -20,6 +23,7 @@ sap.ui.define(
             };
 
         return BaseController.extend("project1.controller.inserisciInvioFirma", {
+            formatter: DateFormatter,
             onInit() {
                 var oProprietà = new JSONModel(),
                     oInitialModelState = Object.assign({}, oData);
@@ -39,10 +43,101 @@ sap.ui.define(
                     "',ZidNi='" + oEvent.getParameters().arguments.campo4 +
                     "',ZRagioCompe='" + oEvent.getParameters().arguments.campo5 + "')"
                 );
-                this.viewHeader(oEvent)
+                this.callPositionNI()
+                //this.viewHeader(oEvent)
             },
 
-            viewHeader: function (oEvent) {
+            HeaderInserisci: function(){
+                
+            },
+
+            callPositionNI: function () {
+
+                var url = location.href
+                var sUrl = url.split("/inserisciInvioFirma/")[1]
+                var aValori = sUrl.split(",")
+
+                var Bukrs = aValori[0]
+                var Gjahr = aValori[1]
+                var Zamministr = aValori[2]
+                var ZchiaveNi = aValori[3]
+                var ZidNi = aValori[4]
+                var ZRagioCompe = aValori[5]
+
+                var filtroNI = []
+                var header = this.getOwnerComponent().getModel("temp").getData().HeaderNISet
+                //var position = this.getView().getModel("temp").getData().PositionNISet
+                for (var i = 0; i < header.length; i++) {
+
+                    if (header[i].Bukrs == Bukrs &&
+                        header[i].Gjahr == Gjahr &&
+                        header[i].Zamministr == Zamministr &&
+                        header[i].ZchiaveNi == ZchiaveNi &&
+                        header[i].ZidNi == ZidNi &&
+                        header[i].ZRagioCompe == ZRagioCompe) {
+
+                        //filtroNI.push({Bukrs:Bukrs, Gjahr:Gjahr, Zamministr,Zamministr, ZchiaveNi:ZchiaveNi, ZidNi:ZidNi, ZRagioCompe:ZRagioCompe})
+                        filtroNI.push(new Filter({
+                            path: "Bukrs",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].Bukrs
+                        }));
+                        filtroNI.push(new Filter({
+                            path: "Gjahr",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].Gjahr
+                        }));
+                        filtroNI.push(new Filter({
+                            path: "Zamministr",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].Zamministr
+                        }));
+                        filtroNI.push(new Filter({
+                            path: "ZchiaveNi",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].ZchiaveNi
+                        }));
+                        filtroNI.push(new Filter({
+                            path: "ZidNi",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].ZidNi
+                        }));
+                        filtroNI.push(new Filter({
+                            path: "ZRagioCompe",
+                            operator: FilterOperator.EQ,
+                            value1: header[i].ZRagioCompe
+                        }));
+                        // filtroNI.push(new Filter({
+                        //     path: "ZposNi",
+                        //     operator: FilterOperator.EQ,
+                        //     value1: ZposNi
+                        // }));
+
+
+                        var that = this;
+                        var oMdlITB = new sap.ui.model.json.JSONModel();
+                        this.getOwnerComponent().getModel().read("/PositionNISet", {
+                            filters: filtroNI,
+                            //filters: [],
+                            urlParameters: "",
+
+                            success: function (data) {
+                                oMdlITB.setData(data.results);
+                                that.getView().getModel("temp").setProperty('/PositionNISet', data.results)
+                                that.viewHeader(data.results)
+                            },
+                            error: function (error) {
+                                var e = error;
+                            }
+                        });
+                        this.getOwnerComponent().setModel(oMdlITB, "HeaderInserisci");
+
+                    }
+                }
+
+            },
+
+            viewHeader: function (position) {
                 // console.log(this.getView().getModel("temp").getData(
                 // "/HeaderNISet('"+ oEvent.getParameters().arguments.campo +
                 // "','"+ oEvent.getParameters().arguments.campo1 +
@@ -50,20 +145,105 @@ sap.ui.define(
                 // "','"+ oEvent.getParameters().arguments.campo3 +
                 // "','"+ oEvent.getParameters().arguments.campo4 +
                 // "','"+ oEvent.getParameters().arguments.campo5 + "')"))
+                var url = location.href
+                var sUrl = url.split("/inserisciInvioFirma/")[1]
+                var aValori = sUrl.split(",")
+
+                var Bukrs = aValori[0]
+                var Gjahr = aValori[1]
+                var Zamministr = aValori[2]
+                var ZchiaveNi = aValori[3]
+                var ZidNi = aValori[4]
+                var ZRagioCompe = aValori[5]
 
                 var header = this.getView().getModel("temp").getData().HeaderNISet
+                var position = position
+                //var valoriNuovi = this.getView().getModel("temp").getData().ValoriNuovi
+                //var ImpegniSelezionati = this.getView().getModel("temp").getData().ImpegniSelezionati
+
                 for (var i = 0; i < header.length; i++) {
-                    if (header[i].Bukrs == oEvent.getParameters().arguments.campo &&
-                        header[i].Gjahr == oEvent.getParameters().arguments.campo1 &&
-                        header[i].Zamministr == oEvent.getParameters().arguments.campo2 &&
-                        header[i].ZchiaveNi == oEvent.getParameters().arguments.campo3 &&
-                        header[i].ZidNi == oEvent.getParameters().arguments.campo4 &&
-                        header[i].ZRagioCompe == oEvent.getParameters().arguments.campo5) {
+                    if (header[i].Bukrs == Bukrs &&
+                        header[i].Gjahr == Gjahr &&
+                        header[i].Zamministr == Zamministr &&
+                        header[i].ZchiaveNi == ZchiaveNi &&
+                        header[i].ZidNi == ZidNi &&
+                        header[i].ZRagioCompe == ZRagioCompe) {
 
+                        var progressivoNI = header[i].ZchiaveNi
+                        this.getView().byId("numNI1").setText(progressivoNI)
 
+                        var dataRegistrazione = header[i].ZdataCreaz
+                        var dataNuova = new Date(dataRegistrazione),
+                            mnth = ("0" + (dataNuova.getMonth() + 1)).slice(-2),
+                            day = ("0" + dataNuova.getDate()).slice(-2);
+                        var nData = [dataNuova.getFullYear(), mnth, day].join("-");
+                        var nDate = nData.split("-").reverse().join(".");
+                        this.getView().byId("dataReg1").setText(nDate)
+
+                        var strAmmResp = header[i].Fistl
+                        this.getView().byId("SARWH2").setText(strAmmResp)
+
+                        var PF = header[i].Fipex
+                        this.getView().byId("pos_FinWH2").setText(PF)
+
+                        var oggSpesa = header[i].ZoggSpesa
+                        this.getView().byId("oggSpesa1").setText(oggSpesa)
+
+                        var mese = header[i].Zmese
+                        this.getView().byId("mese1").setText(mese)
+
+                        for (var x = 0; x < position.length; x++) {
+                            if (position[x].Bukrs == Bukrs &&
+                                position[x].Gjahr == Gjahr &&
+                                position[x].Zamministr == Zamministr &&
+                                position[x].ZchiaveNi == ZchiaveNi &&
+                                position[x].ZidNi == ZidNi &&
+                                position[x].ZRagioCompe == ZRagioCompe) {
+
+                                var comp = position[x].ZcompRes
+                                if (comp == "C") var n_comp = 'Competenza'
+                                if (comp == "R") var n_comp = 'Residui'       //Position
+                                this.getView().byId("comp1").setText(n_comp)
+
+                                var beneficiario = position[x].Lifnr
+                                this.getView().byId("Lifnr1").setText(beneficiario)
+
+                                var centroCosto = position[x].Kostl
+                                this.getView().byId("CentroCosto1").setText(centroCosto)
+
+                                var centroCOGE = position[x].Saknr
+                                this.getView().byId("ConCoGe1").setText(centroCOGE)
+
+                                var codiceGestionale = position[x].Zcodgest
+                                this.getView().byId("CodiceGes1").setText(codiceGestionale)
+
+                                var causalePagamento = position[x].Zcauspag
+                                this.getView().byId("CausalePagamento1").setText(causalePagamento)
+
+                                var modalitàPagamento = position[x].Zwels
+                                this.getView().byId("Zwels1").setText(modalitàPagamento)
+
+                                //var Zattribuito = impegni[o].Zattribuito
+
+                                // var Zcodgest = data[x].Zcodgest
+                                // this.getView().byId("CodiceGes1").setText(Zcodgest)
+
+                                // var Zcauspag = data[x].Zcauspag
+                                // this.getView().byId("CausalePagamento1").setText(Zcauspag)
+                            }
+                        }
+
+                        var statoNI = header[i].ZcodiStatoni
+                        this.getView().byId("statoNI1").setText(statoNI)
+
+                        var importoTot = header[i].ZimpoTotni
+                        this.getView().byId("importoTot1").setText(importoTot) 
+                        this.getView().byId("ImpLiq1").setText(importoTot)
+
+                       
                     }
                 }
-
+                this.HeaderInserisci()
             },
 
             onSelect: function (oEvent) {
@@ -89,7 +269,7 @@ sap.ui.define(
                 this.getOwnerComponent().getRouter().navTo("View1");
             },
 
-            onEditRow: function(){
+            onEditRow: function () {
                 var url = location.href
                 var sUrl = url.split("/inserisciInvioFirma/")[1]
                 var aValori = sUrl.split(",")
